3327ccb90a2c1cb400b9997d2eacb14cfa5745a8
Petr Habrych <p.habrych@seznam.cz>
2025-05-03 01:35:35 +0200

    added functions to library

--- models/book.py
+++ models/book.py
@@ -1,8 +1,30 @@
 class Book:
-    def __init__(self, title, author, year, rating=0, genre):
+    def __init__(self, title, author, year, rating=0, genre, notes, id):
 
         self.title = title
         self.author = author
         self.year = year
         self.rating = rating
         self.genre = genre
+        self.notes = notes
+        self.id = id
+
+    def to_dict(self):
+        return {
+            "title": self.title,
+            "author": self.author,
+            "year": self.year,
+            "rating": self.rating,
+            "genre": self.genre,
+            "notes": self.notes
+        }
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            title = data.get("title", ""),
+            author = data.get("author", ""),
+            year = data.get("year", ""),
+            rating = data.get("rating", ""),
+            genre = data.get("genre", ""),
+            notes = data.get("notes", "")
+        )
